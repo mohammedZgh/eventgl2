f6442079175515d38aafd6c964fd39a3771ec0d3
mohammedZgh <zoughagh.mohammed.1997@gmail.com>
2019-05-23 02:58:46 +0100

    SignUp

---
+++ SignupActivity.java
@@ -0,0 +1,13 @@
+package ensias.um5.com.event_gl2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
